0a68dd99754103010139c280068b32be51c1a5d7
Joed Machado <joedmg58@gmail.com>
2018-09-10 22:13:02 -0400

    New portfolio items added, new script added

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,30 +1,70 @@
 //Variable declarations
 var portfolio = [ 
     {
-        name: "",
+        name: "Hangman",
+        img: "assets/images/hangman.png",
+        href: "https://joedmg58.github.io/Word-Guess-Game/index.html",
+        git: ""
+    },
+    {
+        name: "Crystal collector",
+        img: "assets/images/crystals_shot.png",
+        href: "https://joedmg58.github.io/Crystal-Collector/index.html",
+        git: ""
+    },
+    {
+        name: "Trivia Game",
+        img: "assets/images/Trivia.PNG",
+        href: "https://joedmg58.github.io/TriviaGame/index.html",
+        git: ""
+    },
+    {
+        name: "GifTastic",
+        img: "assets/images/giftastic.gif",
+        href: "https://joedmg58.github.io/GifTastic/index.html",
+        git: ""
+    },
+    {
+        name: "Train Schedule",
+        img: "assets/images/train_schedule.jpg",
+        href: "https://joedmg58.github.io/trainSchedule/index.html"
+    },
+    {
+        name: "Parking Ticket Reinbursement",
+        img: "assets/images/ptr.png",
+        href: "https://joedmg58.github.io/PTR/index.html"
+    },
+    {
+        name: "Rock Paper Scissor",
+        img: "assets/images/rock.PNG",
+        href: "https://joedmg58.github.io/RPS-Game/index.html"
+    },
+    {
+        name: "LIRI-Bot",
+        img: "assets/images/liribot.jpg",
+        href: "",
+        git: "https://github.com/joedmg58/liri-node-app"
+    },
+    {
+        name: "Bamazon",
         img: "",
         href: ""
     },
     {
-        name: "",
+        name: "Friend Finder",
         img: "",
         href: ""
     },
     {
-        name: "",
+        name: "Eat-Da-Burger",
         img: "",
         href: ""
     },
     {
-        name: "",
+        name: "Doula Service",
         img: "",
         href: ""
-    },
-    {
-        name: "",
-        img: "",
-        href: ""
-    } 
+    }  
 ];
 
 //Everythings happends when document is loaded (ready)
@@ -34,7 +74,7 @@ function documentReady( event ) {
 
     console.log( 'document loaded' );
 
-    var portfolioContent = document.getElementsByClassName( "portfolioContent" );
+    var portfolioContent = document.getElementsByClassName( "portfolioContent" )[0];
 
     for( let i = 0; i < portfolio.length; i++ ) {
 
@@ -43,14 +83,23 @@ function documentReady( event ) {
 
         var itemImage = document.createElement( "IMG" ); 
         itemImage.classList.add( "itemPic" );
+        itemImage.setAttribute( 'src', portfolio[i].img );
+        itemImage.setAttribute( 'alt', portfolio[i].name );
+
+        var itemLink = document.createElement( 'a' );
+        itemLink.classList.add( 'itemLink' );
+        itemLink.setAttribute( 'href', portfolio[i].href );
+        itemLink.setAttribute( 'target', '_blank');
+        itemLink.innerHTML = portfolio[i].name;
 
         var itemTitle = document.createElement( "DIV" );
         itemTitle.classList.add( "itemTitle" );
+        itemTitle.appendChild( itemLink );
 
         portfolioItem.appendChild( itemImage );
         portfolioItem.appendChild( itemTitle );
 
-        portfolioContent[0].appendChild( portfolioItem );
+        portfolioContent.appendChild( portfolioItem );
     }
 
 }
